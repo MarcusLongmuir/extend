ab161eb5e479a74da6206d7f589892cabe29ecb7
Marcus Longmuir <marcuslongmuir@me.com>
2014-09-08 22:10:37 +0100

    Initial code commit

---
+++ extend.js
@@ -0,0 +1,13 @@
+//Used to subclass Javascript classes
+function extend(sub, sup) {
+	function emptyclass() {}
+	emptyclass.prototype = sup.prototype;
+	sub.prototype = new emptyclass();
+	sub.prototype.constructor = sub;
+	sub.superConstructor = sup;
+	sub.superClass = sup.prototype;
+}
+
+if (typeof module != 'undefined') {
+    module.exports = extend;
+}
